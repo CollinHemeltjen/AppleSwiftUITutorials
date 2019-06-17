2f2f5b5f3a346517697842a54697ce7aa7631015
Collin Hemeltjen <Collinhemeltjen@gmail.com>
2019-06-17 21:06:59 +0200

    added BuildingListsAndNavigation

---
+++ BuildingListsAndNavigation/Landmarks/LandmarkRow.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Collin Hemeltjen on 16/06/2019.
+//  Copyright © 2019 Collin Hemeltjen. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow : View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image(forSize: 50)
+            Text(landmark.name)
+        }
+    }
+}
+
+#if DEBUG
+struct LandmarkRow_Previews : PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }.previewLayout(.fixed(width: 300, height: 70))
+    }
+}
+#endif
